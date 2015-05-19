e787dfedb92643e86ce5e7acff445ab1f660cbcb
Rebelious <onlyforsupport@mail.ru>
2015-05-19 15:46:40 +0500

    S3 config added to enviroment

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # :registerable, :recoverable,
-  devise :database_authenticatable, :rememberable, :trackable, :validatable# , :registerable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable # , :registerable
 end
