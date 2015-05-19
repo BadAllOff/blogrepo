7396a1ff6ec313f0843636c163bfe3fd165a3234
Rebelious <onlyforsupport@mail.ru>
2015-05-20 02:21:22 +0500

    Test ckeditro again with new RAKE TASK

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,8 @@ set :repo_url, "https://#{ENV['GITHUB_USERNAME']}:#{ENV['GITHUB_PASSWORD']}@gith
 # set :git_https_password, ENV['GITHUB_PASSWORD']
 set :deploy_to, '/opt/www/badblog'
 set :user, 'deploy'
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{public/assets/ckeditor/config.js assets/javascript/ckeditor/contents.css}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle}
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
