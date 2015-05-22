4fa4186ca9c0e78ac300ce906d73b51d02122c4b
Rebelious <onlyforsupport@mail.ru>
2015-05-22 21:21:12 +0500

    Multilang tags not needed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421015026) do
+ActiveRecord::Schema.define(version: 20150522152307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
