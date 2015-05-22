afa4932721fd24b6907e4d108131cad4e9de9804
Rebelious <onlyforsupport@mail.ru>
2015-05-22 20:20:57 +0500

    Last normal state

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,9 +31,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-  config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-  config.assets.precompile += Ckeditor.assets
-  config.assets.precompile += %w(ckeditor/*)
+  # config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+  # config.assets.precompile += Ckeditor.assets
+  # config.assets.precompile += %w(ckeditor/*)
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
