974c98679a5f60133b0583f8ea5f1fae8e668fbc
Rebelious <onlyforsupport@mail.ru>
2015-08-15 19:00:37 +0500

    Changed GitHub link

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'badblog'
-set :repo_url, "https://#{ENV['GITHUB_USERNAME']}:#{ENV['GITHUB_PASSWORD']}@github.com/BadAllOff/badblog5.git" # repo address
+set :repo_url, "https://#{ENV['GITHUB_USERNAME']}:#{ENV['GITHUB_PASSWORD']}@github.com/BadAllOff/badblog.git" # repo address
 # set :git_https_username, ENV['GITHUB_USERNAME']
 # set :git_https_password, ENV['GITHUB_PASSWORD']
 set :deploy_to, '/opt/www/badblog'
