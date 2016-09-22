228814cbfbff12e5d640de512a10e6a7e1a518e2
Badalloff <badalloffsh@gmail.com>
2016-09-22 04:12:24 +0400

    Add: test for articles

--- spec/acceptance/articles/create_article_spec.rb
+++ spec/acceptance/articles/create_article_spec.rb
@@ -11,20 +11,35 @@ feature 'Create Article', '
       sign_in(admin_user)
       visit articles_path
     end
+    context 'with valid attributes' do
+      scenario '- can create article' do
+        click_on 'New Article'
+        fill_in 'article_title', with: 'Test article'
+        fill_in 'article_tags_for_article_list', with: 'Test, tags, 4ever, question body'
+        attach_file 'article_image', "#{Rails.root}/spec/fixtures/css3.jpg"
+        fill_in 'article_preview', with: 'Preview Preview'
+        fill_in 'article_content', with: 'Content Content Content Content Content Content'
+        check 'article_show'
+        click_on 'Create article'
+        click_on 'Log Out'
 
-    scenario '- creates article' do
-      click_on 'New Article'
-      fill_in 'article_title', with: 'Test article'
-      fill_in 'article_tags_for_article_list', with: 'Test, tags, 4ever, question body'
-      attach_file 'article_image', "#{Rails.root}/spec/fixtures/css3.jpg"
-      fill_in 'article_preview', with: 'Preview Preview'
-      fill_in 'article_content', with: 'Content Content Content Content Content Content'
-      check 'article_show'
-      click_on 'Create article'
-      click_on 'Log Out'
+        expect(page).to have_content 'Test article'
+        expect(page).to have_content 'Preview Preview'
+      end
+    end
 
-      expect(page).to have_content 'Test article'
-      expect(page).to have_content 'Preview Preview'
+    context 'with invalid attributes' do
+      scenario "- can't create article" do
+        click_on 'New Article'
+        click_on 'Create article'
+
+        expect(page).to have_content '5 errors prohibited this article from being saved'
+        expect(page).to have_content "Image can't be blank"
+        expect(page).to have_content 'Title is too short (minimum is 1 character)'
+        expect(page).to have_content "Content can't be blank"
+        expect(page).to have_content "Preview can't be blank"
+        expect(page).to have_content "Tags for article list can't be blank"
+      end
     end
   end
 
